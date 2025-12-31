b75724c08f582bb482c6242f624a22e0d8984664
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-31 22:50:08 +0530

    Operations part 1

--- operations_CSV.py
+++ operations_CSV.py
@@ -3,6 +3,12 @@ import pandas as pd
 data = pd.read_csv(r"C:\Users\harsh\Downloads\basic-data.csv")
 
 print(data[["Name","Age"]].head(1))
-print(data.iloc[0])
-print(data.iloc[0:6])
+print(data.iloc[0]) # print entire rows
+print(data.iloc[0:18])
 print(data[data["Age"]==60])
+
+# check for missing value (NULL)
+
+print(data.isnull().sum()) # give total of missing data in csv file
+print(data.dropna()) # remove missing value
+print(data.fillna(0)) # fill mising value
