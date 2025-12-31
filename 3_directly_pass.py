ac039ffc7b345a9452fa691513db91cc4dbe334f
harsh7198 <harshsuradkar2221@gmail.com>
2025-12-31 08:21:44 +0530

    Directly pass in series

---
+++ 3_directly_pass.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+my_series = pd.Series([1,2,3,4,5], index = ['a','b','c','d','e'])
+print(my_series)
+
+import pandas as pd
+
+
+my1_series = pd.Series(25, index = ['a','b','c','d','e']) # repeated value with different index
+print(my1_series)
